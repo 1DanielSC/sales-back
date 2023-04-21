74dfd9ad581adee16f697963b5d85e27ed5b2b70
1danielsc <danielscolao@gmail.com>
2023-04-21 15:01:11 -0300

    build: upgrade project to Java 19 and SpringBoot 3.0.6

--- src/main/java/com/salesback/SalesBackApplication.java
+++ src/main/java/com/salesback/SalesBackApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import brave.sampler.Sampler;
+//COMMENT: ZIPKIN
+//import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,12 +19,12 @@ public class SalesBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SalesBackApplication.class, args);
 	}
-
+/* 
 	@Bean
 	public Sampler defaultSampler(){
 		return Sampler.ALWAYS_SAMPLE;
 	}
-
+*/
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate(){
